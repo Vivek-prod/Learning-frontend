2650df743425075862c6f4568c1f6739f09c257b
Vivek <krishnacreations745@gmail.com>
2026-07-23 20:58:30 +0530

    learned let and const in js

---
+++ js-variable-operators/var.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+let firstname="vivek";
+let lastname="ji";
+
+console.log(firstname);
+console.log(lastname);
+
+let age=70;
+
+console.log(age);
+
+age=700;
+
+console.log(age);
+
+const pi=3.14;
+// pi=10   [will throw an error]
+console.log(pi);
+
+var aege=17;
+// [var is old now let and const used more]
+console.log(aege);
+
+
+console.log((3+2)-76*(1+1));
+
+console.log(1+3);
