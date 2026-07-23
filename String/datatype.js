3fcdfaf9c661941b0ba0f3587d2259107848a697
Vivek <krishnacreations745@gmail.com>
2026-07-24 00:16:44 +0530

    learned datatypes and comparisons

---
+++ String/datatype.js
@@ -0,0 +1,30 @@
+console.log("Hello from Node!");
+console.log("Hello from Node!");
+console.log("Hello from Node!");
+
+const name = "vivek";
+const ne = `hello ${name}`;
+console.log(ne);
+
+console.log(name, ne);
+
+const p = `${name} ${ne}`;
+console.log(p);
+
+// alert(1>2);[give a pop up]
+console.log(1 > 2);
+console.log(true == 1);
+console.log(false == 0);
+
+let a = 0;
+console.log(Boolean(a));
+
+let b = "0";
+console.log(Boolean(b));
+
+console.log(a == b);
+console.log(0 === false);
+console.log("0" === false);
+
+console.log(0 == "");
+console.log(0 === "");
