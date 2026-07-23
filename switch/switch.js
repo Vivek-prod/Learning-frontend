500ca4e8da3b9af5466b009f11d60bbe822f8135
Vivek <krishnacreations745@gmail.com>
2026-07-24 01:33:26 +0530

    learned switch in js

---
+++ switch/switch.js
@@ -0,0 +1,33 @@
+let a = prompt("what is 2+2?", "");
+
+switch (a) {
+  case "3":
+    console.log("3 hoga pagal hai kya");
+    break;
+  case "4":
+    console.log("perfect");
+    break;
+  case "5":
+    console.log("1jayda");
+    break;
+}
+
+switch (a) {
+  case "3":
+    console.log("3 hoga pagal hai kya");
+    break;
+  case "4":
+    console.log("perfect");
+    break;
+  case "5":
+    console.log("1jayda");
+    break;
+
+  case "7":
+  case "6":
+    console.log("math paadhle");
+    console.log("gawr");
+    break;
+  default:
+    console.log("pagal haogya ");
+}
