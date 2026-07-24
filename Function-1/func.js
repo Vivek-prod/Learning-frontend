9258acc57b2415741d2de8a7cba202f16f7c1a3a
Vivek <krishnacreations745@gmail.com>
2026-07-24 19:37:50 +0530

    learned basic of functions

---
+++ Function-1/func.js
@@ -0,0 +1,65 @@
+/*
+function animals(animal) {
+  return animal + " is my favorite animal";
+}
+
+let a = Number(prompt("nuber", ""));
+// let b = +prompt("+", "");[+ = number same work]
+let c = parseInt(prompt("parseint", ""));
+
+// func execute
+const msg = animals(a);
+console.log(msg);
+
+console.log(a);
+
+console.log(c);
+
+
+
+// addition
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+let n1 = Number(prompt("enter your n1:"));
+let n2 = Number(prompt("enter your n2:"));
+const n3 = add(n1, n2);
+console.log("total is:", add(n1, n2));
+console.log(n3);
+
+function hello() {
+  alert("helloooo");
+}
+hello();
+
+
+let n = "p";
+function msg() {
+  let a = "hello";
+  alert(a + n);
+}
+
+msg();
+
+// alert(a);  [wont work as a is local]
+
+
+
+
+let age = 15;
+let agee = 115;
+
+function aege() {
+  age = 20;
+  let agee = 120;
+
+  alert(age);
+  alert(agee);
+}
+aege();
+
+console.log(age);
+console.log(agee);
+*/
